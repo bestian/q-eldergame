3e7db2bd691dde89bcd30aad92b554131a8aeea7
Bestian Tang <bestian@gmail.com>
2019-07-01 15:13:45 +0800

    test

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -1,5 +1,8 @@
 import VueI18n from 'vue-i18n'
 import messages from 'src/i18n'
+import ImageUploader from 'vue-image-upload-resize'
+
+Vue.use(ImageUploader)
 
 export default async ({ app, Vue }) => {
   Vue.use(VueI18n)
