1dec4777a1f78f9bed123e477ca46486ed285bb1
Bestian Tang <bestian@gmail.com>
2019-07-03 08:55:02 +0800

    test

--- quasar.conf.js
+++ quasar.conf.js
@@ -154,7 +154,7 @@ module.exports = function (ctx) {
 
     cordova: {
        id: 'tw.bestian.eldergame',
-       version: '3.0.1',
+       version: '3.1.0',
        noIosLegacyBuildFlag: true, // uncomment only if you know what you are doing
     },
 
