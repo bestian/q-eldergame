aca916860cad7adf4d6b85173fc0ac6b6e36562d
Bestian Tang <bestian@gmail.com>
2020-05-15 21:10:19 +0800

    Revert "Update index.js"
    This reverts commit b20b9ec71a1078f0c4171b817a4db2a8150089ea.

--- src/i18n/en-us/index.js
+++ src/i18n/en-us/index.js
@@ -2,7 +2,7 @@
 // so you can safely delete all default props below
 
 export default {
-  '/': 'Words Game',
+  '/': 'Photo Game',
   '/edit': 'Editor',
   '/contact': 'Contact',
   '/pair': 'Pairing Game',
